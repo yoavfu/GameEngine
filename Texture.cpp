c0321ecf46a0499df532ee75efc70e2c2dc24b4c
yoavfu <yoav.fuchs@gmail.com>
2018-01-08 08:22:41 +0200

    small refactor for texture

--- Texture.cpp
+++ Texture.cpp
@@ -4,21 +4,9 @@
 #include <iostream>
 #include <assert.h>
 
-Texture::Texture(const std::string & fileName, IMAGE_FORMAT imageColors, bool flipImage)
+Texture::Texture(const std::string & fileName, bool flipImage)
 {
-	int width, height, numOfComponents;	
-
-	glGenTextures(1, &textureID_);
-	glBindTexture(GL_TEXTURE_2D, textureID_);
-
-	// set the texture wrapping parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// set texture filtering parameters
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+	int width, height, numOfComponents;
 	// load image, create texture and generate mipmaps
 	stbi_set_flip_vertically_on_load(flipImage);
 	unsigned char* imageData = stbi_load(fileName.c_str(), &width, &height, &numOfComponents, 0);
@@ -26,16 +14,32 @@ Texture::Texture(const std::string & fileName, IMAGE_FORMAT imageColors, bool fl
 	if (imageData == nullptr)
 	{
 		std::cerr << "ERROR: Texture loading failed for texture: " << fileName << std::endl;
+		stbi_image_free(imageData);
 		throw("ERROR: Texture loading failed for texture: " + fileName);
 	}
 	else
 	{
-		unsigned int colorFormat = GetColorFormat(imageColors);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, colorFormat, GL_UNSIGNED_BYTE, imageData);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
+		glGenTextures(1, &textureID_);
 
-	stbi_image_free(imageData);
+		GLenum format;
+		if (numOfComponents == 1)
+			format = GL_RED;
+		else if (numOfComponents == 3)
+			format = GL_RGB;
+		else if (numOfComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID_);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, imageData);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(imageData);
+	}
 }
 
 Texture::~Texture()
@@ -51,12 +55,3 @@ void Texture::Bind(unsigned int unit)
 	glBindTexture(GL_TEXTURE_2D, textureID_);
 }
 
-unsigned int Texture::GetColorFormat(IMAGE_FORMAT imageFormat) const
-{
-	if (imageFormat == RGB)
-		return GL_RGB;
-	else if (imageFormat == RGBA)
-		return GL_RGBA;
-
-	return 0;
-}
--- Texture.h
+++ Texture.h
@@ -4,19 +4,10 @@
 class Texture
 {
 public:
-	enum IMAGE_FORMAT
-	{
-		RGB,
-		RGBA
-	};
-
-	Texture(const std::string& fileName, IMAGE_FORMAT imageColors, bool flipImage = false);
+	Texture(const std::string& fileName, bool flipImage = false);
 	virtual ~Texture();
 
 	void Bind(unsigned int unit);
-
-private:
-	unsigned int GetColorFormat(IMAGE_FORMAT imageFormat) const;
 
 private:
 	unsigned int textureID_;
