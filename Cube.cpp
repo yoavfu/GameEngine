ddda3408335a3637c61c9923c67e75800062eef3
yoavfu <yoav.fuchs@gmail.com>
2018-01-08 08:23:42 +0200

    lighting: ambient, diffuse and specular lighting

--- Cube.cpp
+++ Cube.cpp
@@ -2,23 +2,37 @@
 #include <glad\glad.h>
 #include <iostream>
 
+unsigned int Cube::VBO_ = 0;
+
 Cube::Cube()
 {
 	glGenVertexArrays(1, &VAO_);
-	glGenBuffers(1, &VBO_);
+	bool firstCube = false;
+	if (VBO_ == 0)
+	{
+		glGenBuffers(1, &VBO_);
+		firstCube = true;
+	}
 
 	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
 	glBindVertexArray(VAO_);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_);
-	glBufferData(GL_ARRAY_BUFFER, NUM_OF_CUBE_VERTICES, vertices, GL_STATIC_DRAW);
+
+	if (firstCube)
+		glBufferData(GL_ARRAY_BUFFER, NUM_OF_CUBE_VERTICES, vertices, GL_STATIC_DRAW);
 
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	if (firstCube)
+	{
+		// normal attribute
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+		// tex coords attribute
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+		glEnableVertexAttribArray(2);
+	}
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -31,6 +45,7 @@ Cube::Cube()
 Cube::~Cube()
 {
 	glDeleteVertexArrays(1, &VAO_);
+	glDeleteBuffers(1, &VBO_);
 }
 
 void Cube::Draw()
--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,6 +1,7 @@
 #include "GameEngine.h"
 #include "Shader.h"
 #include "Cube.h"
+#include "Texture.h"
 
 GameEngine::GameEngine() :
 	camera_(glm::vec3(0.0f, 0.0f, 3.0f))
@@ -21,10 +22,19 @@ void GameEngine::GameLoop()
 	Shader lightingShader("Resources/Lighting/1.colors.vs", "Resources/Lighting/1.colors.fs");
 	Shader lampShader("Resources/Lamp/1.lamp.vs", "Resources/Lamp/1.lamp.fs");
 
+	Texture diffuseMap("Resources/container2.png");
+	Texture specularMap("Resources/container2_specular.png");
+
 	Cube cube;
 	Cube lamp;
+	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 	float lastFrame = 0.0f;
+
+	lightingShader.Use();
+	lightingShader.SetInt("material.diffuse", 0);
+	lightingShader.SetInt("material.specular", 1);
+
 	// render loop
 	// -----------
 	while (!display_.ShouldClose())
@@ -36,22 +46,35 @@ void GameEngine::GameLoop()
 		lastFrame = currentFrame;
 
 		//// render
-		display_.Clear(0.2f, 0.3f, 0.3f, 1.0f);
+		display_.Clear(0.1f, 0.1f, 0.1f, 0.1f);
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.Use();
-		lightingShader.SetVec3("objectColor", 1.0f, 0.5f, 0.31f);
-		lightingShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetVec3("light.position", lightPos);
+		glm::vec3 position = camera_.GetPosition();
+		lightingShader.SetVec3("viewPos", position);
 
-		//texture.Bind(0);
-		//texture2.Bind(1);
-		// view/projection/model transformations
+		// light properties
+		lightingShader.SetVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+		lightingShader.SetVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+		lightingShader.SetVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+		// material properties
+		lightingShader.SetFloat("material.shininess", 64.0f);
+
+		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera_.GetZoom()), (float)Display::SCR_WIDTH / (float)Display::SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera_.GetViewMatrix();
-		glm::mat4 model;
 		lightingShader.SetMat4("projection", projection);
 		lightingShader.SetMat4("view", view);
+
 		// world transformation
+		glm::mat4 model;
 		lightingShader.SetMat4("model", model);
+		// bind diffuse map
+		diffuseMap.Bind(0);
+		// bind specular map
+		specularMap.Bind(1);
+		// render the cube
 		cube.Draw();
 
 		// also draw the lamp object
@@ -59,7 +82,10 @@ void GameEngine::GameLoop()
 		lampShader.SetMat4("projection", projection);
 		lampShader.SetMat4("view", view);
 		model = glm::mat4();
-		glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+		// change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
+		lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+		lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
+		model = glm::mat4();
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
 		lampShader.SetMat4("model", model);
