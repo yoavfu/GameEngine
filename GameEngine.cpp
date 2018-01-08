70111a925f0de34647308a6fad935c06a41eb3b7
yoavfu <yoav.fuchs@gmail.com>
2018-01-08 15:40:27 +0200

    support 3 different lights: dir light, point light, spot light

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -35,6 +35,27 @@ void GameEngine::GameLoop()
 	lightingShader.SetInt("material.diffuse", 0);
 	lightingShader.SetInt("material.specular", 1);
 
+	// positions all containers
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+	// positions of the point lights
+	glm::vec3 pointLightPositions[] = {
+		glm::vec3(0.7f,  0.2f,  2.0f),
+		glm::vec3(2.3f, -3.3f, -4.0f),
+		glm::vec3(-4.0f,  2.0f, -12.0f),
+		glm::vec3(0.0f,  0.0f, -3.0f)
+	};
+
 	// render loop
 	// -----------
 	while (!display_.ShouldClose())
@@ -49,14 +70,62 @@ void GameEngine::GameLoop()
 		display_.Clear(0.1f, 0.1f, 0.1f, 0.1f);
 		// be sure to activate shader when setting uniforms/drawing objects
 		lightingShader.Use();
-		lightingShader.SetVec3("light.position", lightPos);
-		glm::vec3 position = camera_.GetPosition();
-		lightingShader.SetVec3("viewPos", position);
-
-		// light properties
-		lightingShader.SetVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-		lightingShader.SetVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
-		lightingShader.SetVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		glm::vec3 cameraPosition = camera_.GetPosition();
+		glm::vec3 cameraFront = camera_.GetFront();
+		lightingShader.SetVec3("light.position", cameraPosition);
+		/*
+		Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index
+		the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
+		by defining light types as classes and set their values in there, or by using a more efficient uniform approach
+		by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
+		*/
+		// directional light
+		lightingShader.SetVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+		lightingShader.SetVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.SetVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+		lightingShader.SetVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+		// point light 1
+		lightingShader.SetVec3("pointLights[0].position", pointLightPositions[0]);
+		lightingShader.SetVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.SetVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.SetVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetFloat("pointLights[0].constant", 1.0f);
+		lightingShader.SetFloat("pointLights[0].linear", 0.09);
+		lightingShader.SetFloat("pointLights[0].quadratic", 0.032);
+		// point light 2
+		lightingShader.SetVec3("pointLights[1].position", pointLightPositions[1]);
+		lightingShader.SetVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.SetVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.SetVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetFloat("pointLights[1].constant", 1.0f);
+		lightingShader.SetFloat("pointLights[1].linear", 0.09);
+		lightingShader.SetFloat("pointLights[1].quadratic", 0.032);
+		// point light 3
+		lightingShader.SetVec3("pointLights[2].position", pointLightPositions[2]);
+		lightingShader.SetVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.SetVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.SetVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetFloat("pointLights[2].constant", 1.0f);
+		lightingShader.SetFloat("pointLights[2].linear", 0.09);
+		lightingShader.SetFloat("pointLights[2].quadratic", 0.032);
+		// point light 4
+		lightingShader.SetVec3("pointLights[3].position", pointLightPositions[3]);
+		lightingShader.SetVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+		lightingShader.SetVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.SetVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetFloat("pointLights[3].constant", 1.0f);
+		lightingShader.SetFloat("pointLights[3].linear", 0.09);
+		lightingShader.SetFloat("pointLights[3].quadratic", 0.032);
+		// spotLight
+		lightingShader.SetVec3("spotLight.position", cameraPosition);
+		lightingShader.SetVec3("spotLight.direction", cameraFront);
+		lightingShader.SetVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+		lightingShader.SetVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.SetFloat("spotLight.constant", 1.0f);
+		lightingShader.SetFloat("spotLight.linear", 0.09);
+		lightingShader.SetFloat("spotLight.quadratic", 0.032);
+		lightingShader.SetFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
 
 		// material properties
 		lightingShader.SetFloat("material.shininess", 64.0f);
@@ -75,7 +144,25 @@ void GameEngine::GameLoop()
 		// bind specular map
 		specularMap.Bind(1);
 		// render the cube
-		cube.Draw();
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model;
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			lightingShader.SetMat4("model", model);
+			cube.Draw();
+		}
+
+		for (unsigned int i = 0; i < 4; i++)
+		{
+			glm::mat4 model;
+			model = glm::translate(model, pointLightPositions[i]);
+			model = glm::scale(model, glm::vec3(0.2f)); // Make it a smaller cube
+			lampShader.SetMat4("model", model);
+			cube.Draw();
+		}
 
 		// also draw the lamp object
 		lampShader.Use();
@@ -83,13 +170,12 @@ void GameEngine::GameLoop()
 		lampShader.SetMat4("view", view);
 		model = glm::mat4();
 		// change the light's position values over time (can be done anywhere in the render loop actually, but try to do it at least before using the light source positions)
-		lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-		lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
-		model = glm::mat4();
+		//lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
+		//lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
 		model = glm::translate(model, lightPos);
 		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
 		lampShader.SetMat4("model", model);
-		lamp.Draw();
+		//lamp.Draw();
 
 		display_.Update();
 	}
